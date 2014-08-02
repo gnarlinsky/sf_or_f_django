0b6d8b2896853cc59571a5527e3f23856d5909cb
gnarlinsky <gnarlinsky@gmail.com>
2014-08-01 20:10:54 -0500

    Added django_social_auth configuration stuff.

--- sff_project/sff_project/settings.py
+++ sff_project/sff_project/settings.py
@@ -36,8 +36,6 @@ STATICFILES_DIRS = (
     ),
 )
 
-# Application definition
-
 INSTALLED_APPS = (
     # Django's
     'django.contrib.auth',
@@ -55,9 +53,83 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 
     # other stuff
-    'debug_toolbar'
+    'debug_toolbar',
+    'social_auth'
 )
 
+
+###############################################################################
+# social_auth
+###############################################################################
+AUTHENTICATION_BACKENDS = (
+#    'social_auth.backends.twitter.TwitterBackend',
+#    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuthBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'social_auth.backends.google.GoogleBackend',
+#    'social_auth.backends.yahoo.YahooBackend',
+#    'social_auth.backends.browserid.BrowserIDBackend',
+#    'social_auth.backends.contrib.linkedin.LinkedinBackend',
+#    'social_auth.backends.contrib.disqus.DisqusBackend',
+#    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
+#    'social_auth.backends.contrib.orkut.OrkutBackend',
+#    'social_auth.backends.contrib.foursquare.FoursquareBackend',
+#    'social_auth.backends.contrib.github.GithubBackend',
+#    'social_auth.backends.contrib.vk.VKOAuth2Backend',
+#    'social_auth.backends.contrib.live.LiveBackend',
+#    'social_auth.backends.contrib.skyrock.SkyrockBackend',
+#    'social_auth.backends.contrib.yahoo.YahooOAuthBackend',
+#    'social_auth.backends.contrib.readability.ReadabilityBackend',
+#    'social_auth.backends.contrib.fedora.FedoraBackend',
+#    'social_auth.backends.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+#TWITTER_CONSUMER_KEY         = ''
+#TWITTER_CONSUMER_SECRET      = ''
+#FACEBOOK_APP_ID              = ''
+#FACEBOOK_API_SECRET          = ''
+#LINKEDIN_CONSUMER_KEY        = ''
+#LINKEDIN_CONSUMER_SECRET     = ''
+#ORKUT_CONSUMER_KEY           = ''
+#ORKUT_CONSUMER_SECRET        = ''
+GOOGLE_CONSUMER_KEY          = ''
+GOOGLE_CONSUMER_SECRET       = ''
+GOOGLE_OAUTH2_CLIENT_ID      = ''
+GOOGLE_OAUTH2_CLIENT_SECRET  = ''
+#FOURSQUARE_CONSUMER_KEY      = ''
+#FOURSQUARE_CONSUMER_SECRET   = ''
+#VK_APP_ID                    = ''
+#VK_API_SECRET                = ''
+#LIVE_CLIENT_ID               = ''
+#LIVE_CLIENT_SECRET           = ''
+#SKYROCK_CONSUMER_KEY         = ''
+#SKYROCK_CONSUMER_SECRET      = ''
+#YAHOO_CONSUMER_KEY           = ''
+#YAHOO_CONSUMER_SECRET        = ''
+#READABILITY_CONSUMER_SECRET  = ''
+#READABILITY_CONSUMER_SECRET  = ''
+
+LOGIN_URL          = '/login-form/'
+LOGIN_REDIRECT_URL = '/logged-in/'
+LOGIN_ERROR_URL    = '/login-error/'
+
+# A different URL could be defined for newly registered users, newly
+# associated accounts, or account disconnections
+#SOCIAL_AUTH_NEW_ASSOCIATION_REDIRECT_URL = '/new-association-redirect-url/'
+#SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/new-users-redirect-url/'
+#SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/account-disconnected-redirect-url/'
+
+# Configure authentication and association complete URL names to avoid possible clashes:
+SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
+SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
+
+# Inactive users can be redirected to a different page if this setting is defined:
+SOCIAL_AUTH_INACTIVE_USER_URL = '...'
+
+###############################################################################
+# Django Suit
+###############################################################################
 # Django Suit configuration example
 SUIT_CONFIG = {
     # header
