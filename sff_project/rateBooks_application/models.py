3cea3dacb4e6f0a3e157e2a1222b694abb5c76b3
gnarlinsky <gnarlinsky@gmail.com>
2014-08-05 19:41:11 -0500

    Added Vote model; so no more votes_sf, votes_f attributes on Book model.

--- sff_project/rateBooks_application/models.py
+++ sff_project/rateBooks_application/models.py
@@ -1,4 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Vote(models.Model):
+    # TODO: best practices, etc for arguments passed to each kind of Field
+    SCIENCE_FICTION = 'SF'
+    FANTASY = 'F'
+    VOTE_CHOICES = (
+        (SCIENCE_FICTION, 'Science Fiction'),
+        (FANTASY, 'Fantasy'),
+    )
+    vote = models.CharField(max_length=2, choices=VOTE_CHOICES, null=False)
+    book = models.ForeignKey('Book', null=False)
+    user = models.ForeignKey(User, null=False)
+    # TODO: user together with book should be unique
+
+    @staticmethod
+    def user_voted_for_book(user, book):
+        if Vote.objects.filter(user=user, book=book):
+            return True;
+        else:
+            return False
+
+    @staticmethod
+    def user_voted_f_for_book(user, book):
+        vote = Vote.objects.filter(user=user, book=book).first()
+        if vote != None and vote.vote == Vote.FANTASY:
+           return True
+        else:
+            return False
+
+    @staticmethod
+    def user_voted_sf_for_book(user, book):
+        vote = Vote.objects.filter(user=user, book=book).first()
+        if vote != None and vote.vote == Vote.SCIENCE_FICTION:
+           return True
+        else:
+            return False
 
 class Book(models.Model):
     # TODO: best practices, etc for arguments passed to each kind of Field
@@ -8,12 +45,15 @@ class Book(models.Model):
     #   is required in forms. So the combo of null=True, blank=False
     #   means that the field should be required in forms, but not if
     #   the object is created programmatically/in the db, etc.
+
+    # TODO: it's not really the title that should be unique -- it should be the
+    # title together with author that should be unique.
     title   = models.CharField(max_length=255, blank=False, null=False, unique=True)
     author  = models.CharField(max_length=255, blank=False, null=False)
 
     # number of votes in each category
-    votes_sf    = models.IntegerField(null=True, editable=False, default=0)
-    votes_f     = models.IntegerField(null=True, editable=False, default=0)
+    #votes_sf    = models.IntegerField(null=True, editable=False, default=0)
+    #votes_f     = models.IntegerField(null=True, editable=False, default=0)
 
     ###########################################################################
     # "I would opt for the 0..1 range simply because if you store e.g. 20% as
@@ -46,25 +86,39 @@ class Book(models.Model):
                            self.author ]
                         )
 
+    def get_num_sf_votes(self):
+        votes = Vote.objects.filter(book=self, vote=Vote.SCIENCE_FICTION)
+        return len(votes)
+
+    def get_num_f_votes(self):
+        votes = Vote.objects.filter(book=self, vote=Vote.FANTASY)
+        return len(votes)
+
     def get_percentage_f_votes(self):
         """ Return percentage of fantasy votes as a float between 0 and 100,
             to 1 decimal places. """
 
-        print '****'*100
+        votes_f = self.get_num_f_votes()
+        votes_sf = self.get_num_sf_votes()
+
         # avoid dividing by zero
-        if self.votes_f + self.votes_sf == 0:
+        if votes_f + votes_sf == 0:
             return 0
         else:
-            ratio = float(self.votes_f)/(self.votes_f + self.votes_sf)
+            ratio = float(votes_f)/(votes_f + votes_sf)
             return round(ratio * 100, 1)
+
 
     def get_percentage_sf_votes(self):
         """ Return percentage of science fiction votes as a float between 0 and 100,
             to 1 decimal places. """
 
+        votes_f = self.get_num_f_votes()
+        votes_sf = self.get_num_sf_votes()
+
         # avoid dividing by zero
-        if self.votes_f + self.votes_sf == 0:
+        if votes_f + votes_sf == 0:
             return 0
         else:
-            ratio = float(self.votes_sf)/(self.votes_f + self.votes_sf)
+            ratio = float(votes_sf)/(votes_f + votes_sf)
             return round(ratio * 100, 1)
