312450830bd799926765349d480cbb0c8e3d572c
gnarlinsky <gnarlinsky@gmail.com>
2014-08-10 20:36:11 -0500

    Commented out all Django debug toolbar stuff, to hopefully prevent a weird urlconf error on heroku.

--- sff_project/sff_project/settings_production.py
+++ sff_project/sff_project/settings_production.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 
     # other stuff
-    'debug_toolbar',
+    #'debug_toolbar',
     'social_auth',
 
     # heroku
@@ -180,21 +180,23 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP + (
 ###############################################################################
 # Debug-toolbar settings
 ###############################################################################
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
+#DEBUG_TOOLBAR_PANELS = [
+#    'debug_toolbar.panels.versions.VersionsPanel',
+#    'debug_toolbar.panels.timer.TimerPanel',
+#    'debug_toolbar.panels.settings.SettingsPanel',
+#    'debug_toolbar.panels.headers.HeadersPanel',
+#    'debug_toolbar.panels.request.RequestPanel',
+#    'debug_toolbar.panels.sql.SQLPanel',
+#    'debug_toolbar.panels.templates.TemplatesPanel',
+#    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#    'debug_toolbar.panels.cache.CachePanel',
+#    'debug_toolbar.panels.signals.SignalsPanel',
+#    'debug_toolbar.panels.logging.LoggingPanel',
+#    'debug_toolbar.panels.redirects.RedirectsPanel',
+#    'debug_toolbar.panels.profiling.ProfilingPanel',
+#]
+## may prevent weird urlconf errors on heroku
+#DEBUG_TOOLBAR_PATCH_SETTINGS = False 
 
 
 MIDDLEWARE_CLASSES = (
